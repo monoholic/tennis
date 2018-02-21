2d3bf8424922abfcacc9beb662e320f87f109a11
monoholic <monoholic00@gmail.com>
2018-02-22 01:37:46 +0900

    1. 상세화면 수정 2. 대회등록 파라미터 추가

--- theme/companyBts2/pages/schedule/schedule_index.php
+++ theme/companyBts2/pages/schedule/schedule_index.php
@@ -1,18 +1,33 @@
 <!-- 홈 배너 -->
 <script src="<?php echo G5_THEME_URL; ?>/pages/schedule/schedule.js"></script>
 <link rel="stylesheet" href="<?php echo G5_THEME_CSS_URL; ?>/schedule.css">
+<?php
+
+error_reporting(E_ALL);
+
+ini_set("display_errors", 1);
+
+    $sql = "select * ";
+    $sql .= " ,replace(competition_caution,'\r\n','</BR>') as competition_caution";
+    $sql .= " ,replace(common_standard,'\r\n','</BR>') as common_standard";
+    $sql .= " ,replace(participant_caution,'\r\n','</BR>') as participant_caution";
+    $sql .= " from {$g5['competition_table']} where competition_id =".$competition_id;
+
+    echo $competition_id;
+    $comp = sql_fetch($sql);
+?>
 <section id="game-detail">
     <div class="container flexbox flow-col align-center">
         <div class="title-box flexbox flow-col align-center">
             <div class="due-date-box flexbox just-center">
                 <span class="due-date-label">접수마감</span>
-                <span class="due-date">2018.12.31(일) PM 6:00</span>
+                <span class="due-date"><?php echo $comp['competition_deadline'].' '.$comp['competition_deadline_time']?></span>
             </div>
             <div class="game-title">
-                <span>제 100회 프리다이빙 대회</span>
+                <span><?php echo $comp['competition_title']?></span>
             </div>
             <div class="part-title">
-                <span>혼합복식부 지역 신인부(남자)</span>
+                <span><?php echo $comp['competition_subtitle']?></span>
             </div>
         </div>
         <div class="game-content flexbox just-between">
@@ -21,25 +36,38 @@
                 <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=2f17c15d3dc2614a3da80008b5b1d91a"></script>
                 <script>
                     var container = document.getElementById('map');
+                    var lng = '<?php echo $comp['competition_longitude'] ?>';
+                    var lat = '<?php echo $comp['competition_latitude'] ?>';
                     var options = {
-                        center: new daum.maps.LatLng(<?php echo 37.5662952 ?>, <?php echo 126.9779451 ?>),
+                        center: new daum.maps.LatLng(lat,lng),
                         level: 3
                     };
 
                     var map = new daum.maps.Map(container, options);
 
+                    // 마커가 표시될 위치입니다 
+                    var markerPosition  = new daum.maps.LatLng(lat, lng); 
+                    var coords = new daum.maps.LatLng(lat, lng);
+                    // 마커를 생성합니다
+                    var marker = new daum.maps.Marker({
+                        position: markerPosition
+                    });
+
+                    marker.setMap(map);
+                    map.setCenter(coords);
+
                     function setZoomable(zoomable) {
                         // 마우스 휠로 지도 확대,축소 가능여부를 설정합니다
                         map.setZoomable(zoomable);    
                     }
-
                     setZoomable(false);
                 </script>
+
                 <div class="complex-address flexbox align-center just-center">
                     <div class="addr-img">
                     </div>
                     <div>
-                        <span>사천시 수청테니스장외 보조구장</span>
+                        <span><?php echo $comp['competition_address'] ?></span>
                     </div> 
                     <div class="copy-addr-btn flexbox just-center">
                         <span>주소복사</span>
@@ -50,35 +78,40 @@
                 <ul class="game-info flexbox flow-col just-between">
                     <li class="df-row">
                         <span class="df-row-label">일시</span>
-                        <span>2019.01.01(월) ~ 14(일) 2주간</span>
+                        <span><?php echo $comp['competition_schedule_from'].' '.$comp['competition_schedule_to']?></span>
                     </li>
                     <li class="df-row">
                         <span class="df-row-label">주최</span>
-                        <span>경남 필, 빅토리아 운영위원회</span>
+                        <span><?php echo $comp['competition_host']?></span>
                     </li>
                     <li class="df-row">
                         <span class="df-row-label">주관</span>
-                        <span>테니스나라</span>
+                        <span><?php echo $comp['competition_subj']?></span>
                     </li>
                     <li class="df-row">
                         <span class="df-row-label">후원</span>
-                        <span>사천테니스협회, 동광플라워, 프로켄넥스, KT사천대리점</span>
+                        <span><?php echo $comp['competition_support']?></span>
                     </li>
                     <li class="df-row">
                         <span class="df-row-label">협찬</span>
-                        <span>아머스포츠코리아, 윌슨</span>
+                        <span><?php echo $comp['competition_sponsor']?></span>
                     </li>
                     <li class="df-row">
                         <span class="df-row-label">참가상품</span>
-                        <span>지역 특산품</span>
+                        <span><?php echo $comp['competition_goods']?></span>
                     </li>
                     <li class="df-row">
                         <span class="df-row-label">참가비</span>
-                        <span>팀당</span><span>52,000</span><span>원</span><span>(유소년 육성기금 2,000원 포함)</span>
+                        <span>팀당</span><span><?php echo $comp['competition_fee']?></span><span>원</span>
+
+                        <?php if($comp['competition_youth_fund'] != ''){?>
+                            <span>(유소년 육성기금 2,000원 포함)</span>
+                        <?php } ?>
+
                     </li>
                     <li class="df-row">
                         <span class="df-row-label">참가팀</span>
-                        <span>100</span><span>/</span><span>23</span><span>팀 접수중</span>
+                        <span><?php echo $comp['competition_teamcnt']?></span><span>/</span><span>23</span><span>팀 접수중</span>
                     </li>
                 </ul>
                 <div class="btns-area flexbox" style="margin-top: 50px">
@@ -94,8 +127,7 @@
                         <span>* 참가신청시 주의사항</span>
                     </div>
                     <div class="pre-caution-descript">
-                        - 사이트에서만 접수가능하며 신청 마감일까지 참가비 미입금자는 대진표에서 제외</br>
-                        - 마감 후 환불 불가하며 참가시 상품 지급
+                        <?php echo $comp['competition_caution']?>
                     </div>
                 </div>
             </div>
@@ -120,25 +152,25 @@
                         <div class="reward-box flexbox flow-col align-center">
                             <div class="reward-img-1"></div>
                             <div class="reward-name">우승</div>
-                            <div class="reward-gift">상패 및 100만원</div>
+                            <div class="reward-gift"><?php echo $comp['goods_first']?></div>
                         </div>
                         <div class="reward-box flexbox flow-col align-center">
                             <div class="reward-img-1"></div>
-                            <div class="reward-name">우승</div>
-                            <div class="reward-gift">상패 및 100만원</div>
+                            <div class="reward-name">준우승</div>
+                            <div class="reward-gift"><?php echo $comp['goods_second']?></div>
                         </div>
                         <div class="reward-box flexbox flow-col align-center">
                             <div class="reward-img-1"></div>
-                            <div class="reward-name">우승</div>
-                            <div class="reward-gift">상패 및 100만원</div>
+                            <div class="reward-name">4강</div>
+                            <div class="reward-gift"><?php echo $comp['goods_third']?></div>
                         </div>
                         <div style="width:2px; margin-left: 70px;" class="flexbox just-center">
                             <div class="div-line-v" style="height:73px; margin-bottom:44px;"></div>
                         </div>
                         <div class="reward-box flexbox flow-col align-center">
                             <div class="reward-img-1"></div>
-                            <div class="reward-name">우승</div>
-                            <div class="reward-gift">상패 및 100만원</div>
+                            <div class="reward-name">8강</div>
+                            <div class="reward-gift"><?php echo $comp['goods_quater']?></div>
                         </div>
                     </div>
                     <div class="reward-desc">
@@ -158,30 +190,30 @@
                     <tr class="col-type-1">
                         <td>
                             <div class="flexbox flow-col align-center">
-                                <span>2017. 12. 17 (금) AM 9:00</span>
+                                <span><?php echo $comp['major_schedule'] .' '.$comp['major_schedule_time']?></span>
                                 <div class="div-line-h" style="width: 13px; margin: 8px 0;"></div>
-                                <span>코트 추후 안내 예정</span>
+                                <span><?php echo $comp['major_court']?></span>
                             </div>
                         </td>
                         <td>
                             <div class="flexbox flow-col align-center">
-                                <span>2017. 12. 17 (금) AM 9:00</span>
+                                <span><?php echo $comp['tour_schedule'] .' '.$comp['tour_schedule_time']?></span>
                                 <div class="div-line-h" style="width: 13px; margin: 8px 0;"></div>
-                                <span>코트 추후 안내 예정</span>
+                                <span><?php echo $comp['tour_court']?></span>
                             </div>
                         </td>
                         <td>
                             <div class="flexbox flow-col align-center">
-                                <span>2017. 12. 17 (금) AM 9:00</span>
+                                <span><?php echo $comp['challenger_schedule'] .' '.$comp['challenger_schedule_time']?></span>
                                 <div class="div-line-h" style="width: 13px; margin: 8px 0;"></div>
-                                <span>코트 추후 안내 예정</span>
+                                <span><?php echo $comp['challenger_court']?></span>
                             </div>
                         </td>
                         <td>
                             <div class="flexbox flow-col align-center">
-                                <span>2017. 12. 17 (금) AM 9:00</span>
+                                <span><?php echo $comp['circuit_schedule'] .' '.$comp['circuit_schedule_time']?></span>
                                 <div class="div-line-h" style="width: 13px; margin: 8px 0;"></div>
-                                <span>코트 추후 안내 예정</span>
+                                <span><?php echo $comp['circuit_court']?></span>
                             </div>
                         </td>
                     </tr>
@@ -195,10 +227,10 @@
                         <th>서킷</th>
                     </tr>
                     <tr class="col-type-3">
-                        <td>만 50세 이상</td>
-                        <td>만 50세 이상</td>
-                        <td>만 50세 이상</td>
-                        <td>만 50세 이상</td>
+                        <td>만 <?php echo $comp['major_age']?>세 이상</td>
+                        <td>만 <?php echo $comp['tour_age']?>세 이상</td>
+                        <td>만 <?php echo $comp['challenger_age']?>세 이상</td>
+                        <td>만 <?php echo $comp['circuit_age']?>세 이상</td>
                     </tr>
                 </table>
                 <table class="table game-schd-table">
@@ -206,76 +238,60 @@
                         <th>공통부분</th>
                     </tr>
                     <tr class="col-type-4">
-                        <td>- 연령은 주민등록상의 연도만 적용</br>- 연령은 주민등록상의 연도만 적용</br>- 연령은 주민등록상의 연도만 적용</br>- 연령은 주민등록상의 연도만 적용</br>- 연령은 주민등록상의 연도만 적용</td>
+                        <td>
+                            <?php echo $comp['common_standard']?>
+                        </td>
                     </tr>
                 </table>
             </div>  
             <div id="game-3" class="content-box">
                 <div class="tab-sub-title"><span>진행방식</span></div>
                 <div class="border-box">
-                    - 예선은 조별리그로 하고</br>
-                    - 예선은 조별리그로 하고</br>
-                    - 예선은 조별리그로 하고</br>
-                    - 예선은 조별리그로 하고</br>
-                    - 예선은 조별리그로 하고</br>
+                <?php echo $comp['competition_proceed']?>
                 </div>
                 <div class="gray-box">
-                    예선리그 성적우선순위</br>
-                    가)승률</br>
-                    가)승률</br>
-                    가)승률</br>
-                    가)승률</br>
-                    가)승률</br>
-                    가)승률</br>
-                    가)승률</br>
-                    가)승률</br>
-                    가)승률</br>
-                    가)승률</br>
+                    <?php echo $comp['competition_priority1']?>
                 </div>
                 <div class="tab-sub-title"><span>경기순서</span></div>
                 <div class="border-box">
-                    * 3팀일 경우 : 1-2, 3-(1-2번승자), 3-(1-2번패자)</br>
-                    * 4팀일 경우 : 1-4, 2-3, 2-4, 3-4</br>
+                    <?php echo $comp['competition_priority2']?>
+                    </br>
+                    <?php echo $comp['competition_priority3']?>
+                    </br>
                 </div>
                 <div class="grame-seq-content gray-box flexbox flow-col align-center">
-                    <div>1코트에 2개조가 들어갈 경우</div>
+                    <div><?php echo $comp['competition_priority4']?></div>
                     <div class="game-seq-court flexbox align-center just-between">
                         <div class="court flexbox flow-col align-center just-between">
                             <div class="court-img"></div>
-                            <div><span>홀수조 1-2, 3-(1-2번승자), 3-(1-2번패자)</span></div>
+                            <div><span><?php echo $comp['competition_priority5']?></span></div>
                         </div>
                         <div class="court flexbox flow-col align-center just-between">
                             <div class="court-img"></div>
-                            <div><span>홀수조 1-2, 3-(1-2번승자), 3-(1-2번패자)</span></div>
+                            <div><span><?php echo $comp['competition_priority6']?></span></div>
                         </div>
                     </div>
                 </div>
                 <div class="tab-sub-title"><span>참가자 준수사항</span></div>
                 <div class="border-box">
-                    - 부서별 연령기준은 연도만 적용한다.</br>
-                    - 부서별 연령기준은 연도만 적용한다.</br>
-                    - 부서별 연령기준은 연도만 적용한다.</br>
-                    - 부서별 연령기준은 연도만 적용한다.</br>
-                    - 부서별 연령기준은 연도만 적용한다.</br>
-                    - 부서별 연령기준은 연도만 적용한다.</br>
-                    - 부서별 연령기준은 연도만 적용한다.</br>
-                    - 부서별 연령기준은 연도만 적용한다.</br>
-                    - 부서별 연령기준은 연도만 적용한다.</br>
+                    <?php echo $comp['participant_caution']?>
                 </div>
             </div>  
             <div id="game-4" class="content-box">
                 <div class="game-helper user-list flexbox align-start just-between">
                     <?php
-                    for ($i=0; $i<4; $i++) {
+                    $sql = "select * from {$g5['game_helper_table']} where competition_id =".$competition_id;
+                    $result = sql_query($sql, false);
+                    for ($i=0; $row=sql_fetch_array($result); $i++){
                     ?>
                     <div class="user-box flexbox flow-col align-center just-between">
                         <div class="info flexbox flow-col align-center just-between">
                             <div class="photo"></div>
-                            <span class="name" >안정민</span>
-                            <span class="belong">분당알파</span>
+                            <span class="name" ><?php echo $row['helper_name']?></span>
+                            <span class="belong"><?php echo $row['helper_belong']?></span>
                         </div>
                         <div class="info-box flexbox flow-col just-center">
-                            <span class="phone-number">010-1234-1234</span>
+                            <span class="phone-number"><?php echo $row['helper_phone']?></span>
                         </div>
                     </div>
                     <?php } ?>
