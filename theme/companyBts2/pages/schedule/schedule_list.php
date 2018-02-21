49731527a6f6b78949e46d3f3cb3f3550a5f4106
monoholic <monoholic00@gmail.com>
2018-02-21 21:25:22 +0900

    대회스케쥴 수정

--- theme/companyBts2/pages/schedule/schedule_list.php
+++ theme/companyBts2/pages/schedule/schedule_list.php
@@ -1,4 +1,13 @@
 <link rel="stylesheet" href="<?php echo G5_THEME_CSS_URL; ?>/schedule_list.css">
+<script type="text/javascript">
+    function goDetail(){
+        location.href="./schedule.php";
+    }
+
+    function addGame(){
+        location.href = "./schedule_admin.php";
+    }
+</script>
 <section id="banner" class="container">
     <div class="banner-box">
         <div class="banner banner-1"></div>
@@ -13,23 +22,27 @@
             <li class="" onclick="onClickListTab(this)"><a>지난 대회</a></li>
         </ul>
         <div class="games-box flexbox">
-            <?php
-                for ($i=0; $i<6; $i++) {
-            ?>
-                <div class="game-box flexbox flow-col align-start just-end">
+        <?php 
+                $sql = " select * from {$g5['competition_table']}";
+                $result = sql_query($sql, false);
+                for ($i=0; $row=sql_fetch_array($result); $i++){
+                ?>
+
+                <div class="game-box flexbox flow-col align-start just-end" style="cursor:pointer;" onclick="goDetail();">
                     <div class="game-info">
                         <div style="font-size:18px;">2018.01.01 ~ 2018.01.18</div>
-                        <div style="font-size:26px; margin-top: 8px;">제 100회 다이빙대회</div>
+                        <div style="font-size:26px; margin-top: 8px;"><?php echo $row['competition_id'] ?></div>
                         <div style="font-size:13px; margin-top: 8px;">수지구청 스포츠센터 다이빙 풀</div>
                         <div class="game-owner">
                             <span style="font-size:13px;">주최</span>
                             <span style="font-size:13px; font-weight:bold;">인투터블루</span>
                             <span style="font-size:13px;">주관</span>
                             <span style="font-size:13px; font-weight:bold;">스쿠버블</span>
-                        </div>
+                      </div>
                     </div>
                     <div style="width:100px; height:100px; background: orange; position:absolute; top:0px; right: 0px;"></div>
                 </div>
+
             <?php } ?>
         </div>
         <div class="page-box flexbox align-center just-between">
